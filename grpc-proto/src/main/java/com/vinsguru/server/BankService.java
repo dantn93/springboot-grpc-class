e9348dabd2a8c92e4b203725944bc0d3512cc59f
dantran <dantran@lecle.co.kr>
2023-11-07 01:20:54 +0700

    10: server streaming validation error handling

--- grpc-proto/src/main/java/com/vinsguru/server/BankService.java
+++ grpc-proto/src/main/java/com/vinsguru/server/BankService.java
@@ -1,8 +1,7 @@
 package com.vinsguru.server;
 
-import com.vinsguru.models.Balance;
-import com.vinsguru.models.BalanceCheckRequest;
-import com.vinsguru.models.BankServiceGrpc;
+import com.vinsguru.models.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -16,6 +15,28 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                 .build();
 
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<com.vinsguru.models.Money> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        int amount = request.getAmount();
+        int balance = AccountDatabase.getBalance(accountNumber);
+
+        if(balance < amount) {
+            Status status = Status.FAILED_PRECONDITION.withDescription("No enough money. You have only " + balance);
+            responseObserver.onError(status.asRuntimeException());
+            return;
+        }
+
+        // all the validations passed
+        for (int i = 0; i < (amount / 10); i++) {
+            Money money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+            AccountDatabase.deductBalance(accountNumber, 10);
+        }
+
         responseObserver.onCompleted();
     }
 }
