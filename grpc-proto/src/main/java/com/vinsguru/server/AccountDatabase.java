c7821543c07bea8ea35a2d60cf6be1a244060746
dantran <dantran@lecle.co.kr>
2023-11-07 12:43:52 +0700

    15: bi-directional streaming rpc

--- grpc-proto/src/main/java/com/vinsguru/server/AccountDatabase.java
+++ grpc-proto/src/main/java/com/vinsguru/server/AccountDatabase.java
@@ -15,7 +15,7 @@ public class AccountDatabase {
     private static final Map<Integer, Integer> MAP = IntStream
             .rangeClosed(1, 10)
             .boxed()
-            .collect(Collectors.toMap(Function.identity(), v -> v * 10)
+            .collect(Collectors.toMap(Function.identity(), v -> 100)
             );
 
 
@@ -29,6 +29,12 @@ public class AccountDatabase {
 
     public static Integer deductBalance(int accountId, int amount) {
         return MAP.computeIfPresent(accountId, (k, v) -> v - amount);
+    }
+
+    public static void printAccountDetails() {
+        System.out.println(
+                MAP
+        );
     }
 
 
